bc2c9a4442d282666a760ee1cc7e43bb32a969a9
Lawrence Latif <public@lawrencelatif.com>
2021-05-17 19:17:44 -0700

    Split into modules

--- pingstat.py
+++ pingstat.py
@@ -1,29 +1,5 @@
-import sqlite3
+import sqlops as sops
 
-from sqlite3 import Error
-from sqlite3.dbapi2 import SQLITE_CREATE_TABLE
+if __name__ == '__main__':
 
-def createSqlLiteDb():
-# Creates a SQLite3 database in memory
-
-    conn = None;
-    try:
-        conn = sqlite3.connect(':memory:')
-        print(sqlite3.version)
-        print("SQLite database created")
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-
-def createTable():
-# DB schema for files
-
-  fileDbSchema = """ CREATE TABLE IF NOT EXISTS files (
-                    id integer PRIMARY KEY,
-                    path text NOT NULL,
-                    filetype text NOT NULL
-                 ); """
-
-  create_table()
+  sops.createSqlLiteDb()
---
+++ sqlops.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+from sqlite3 import Error
+from sqlite3.dbapi2 import SQLITE_CREATE_TABLE
+
+def createSqlLiteDb():
+# Creates a SQLite3 database in memory
+
+    conn = None;
+    fileDbSchema = """ CREATE TABLE IF NOT EXISTS latency (
+                    id integer PRIMARY KEY,
+                    source text NOT NULL,
+                    destination text NOT NULL,
+                    latency int NOT NULL
+                 ); """
+   
+    try:
+
+        conn = sqlite3.connect(':memory:')
+        print(sqlite3.version)
+        print("SQLite database created")
+        
+        cursor = conn.cursor()
+        cursor.execute(fileDbSchema)
+        print("Database table created")
+
+        cursor.execute('''INSERT INTO latency(source, destination, latency)
+                          VALUES('192.168.0.1', '192.168.1.1', 2),
+                          ('192.168.0.1', '192.168.1.2', 4)''')
+        conn.commit()
+    
+        cursor.execute('''SELECT * FROM latency''')
+        result = cursor.fetchall();
+        print(result)
+    
+    except Error as e:
+
+        print(e)
+    
+    finally:
+        
+        if conn:
+            conn.close()
